8f20f672fa6c6a602657d5642329afda71e18b76
dustin-py <dustinstringer92@gmail.com>
2020-09-03 09:23:46 -0700

    added cardinality_cutter funtion to johnWayne class in wild_west module

--- dustydata/wild_west.py
+++ dustydata/wild_west.py
@@ -3,6 +3,12 @@ import pandas as pd
 import numpy as np
 
 class johnWayne:
+    """
+    Data Wrangling Class
+    
+    ex:
+    new_wrangler = johnWayne(dataframe)
+    """
     
     def __init__(self, dataframe, check_null=True, 
                  check_data_types=True):
@@ -11,7 +17,23 @@ class johnWayne:
         self.check_data_types = check_data_types
         self.main()
         
+    def main(self):
+        """
+        Main function to run when the class is instantiated.
+        These functions can be enabled and disabled as params in the
+        class instance.
+        """
+        if self.check_null == True:
+            self.__check_nulls__()
+            
+        if self.check_data_types == True:
+            self.__check_data_types__()
+        
     def __check_nulls__(self):
+        """
+        Checks for null values in a data frame and 
+        returns the sum of nulls in a column.
+        """
         null_dictionary = {}
         for e, i in enumerate(self.dataframe.isnull().sum()):
             if i > 0:
@@ -19,13 +41,19 @@ class johnWayne:
         print(null_dictionary)
         
     def __check_data_types__(self):
+        """
+        Returns the datatypes of each series in a data frame.
+        """
         print(self.dataframe.dtypes)
         
-    def main(self):
-        if self.check_null == True:
-            self.__check_nulls__()
-        if self.check_data_types == True:
-            self.__check_data_types__()
+    def cardinality_cutter(self, threshold=50):
+        """
+        Cut the cardinality of categorical data.
+        Return a list of features that made the cut.
+        """
+        card = self.dataframe.select_dtypes(exclude='number').nunique()
+        card_cut = card[card <= threshold].index.tolist()
+        return card_cut 
 
 
 df = pd.DataFrame({0: [1, 2.9, 3, 4, 5, 6, 7],
@@ -34,7 +62,8 @@ df = pd.DataFrame({0: [1, 2.9, 3, 4, 5, 6, 7],
                     3: ['t', np.NaN, 'f', 'f', 't', 'h', 'j'],
                     4: ['f', 'h', 'd', 'k', 'j', 'y', 'j']})
 
-jw = johnWayne(df)
+jw = johnWayne(df, False, False)
+print(jw.cardinality_cutter(threshold=5))
         
     
         
