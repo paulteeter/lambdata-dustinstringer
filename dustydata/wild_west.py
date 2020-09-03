07a288ded6bac1be68e9b150423a3e4f747cd3ee
dustin-py <dustinstringer92@gmail.com>
2020-09-03 09:12:15 -0700

    added __check_data_types__ function to johnWayne class in wild_west module

--- dustydata/wild_west.py
+++ dustydata/wild_west.py
@@ -4,9 +4,11 @@ import numpy as np
 
 class johnWayne:
     
-    def __init__(self, dataframe, check_null=True):
+    def __init__(self, dataframe, check_null=True, 
+                 check_data_types=True):
         self.dataframe = dataframe
         self.check_null = check_null
+        self.check_data_types = check_data_types
         self.main()
         
     def __check_nulls__(self):
@@ -16,18 +18,23 @@ class johnWayne:
                 null_dictionary[e] = i
         print(null_dictionary)
         
+    def __check_data_types__(self):
+        print(self.dataframe.dtypes)
+        
     def main(self):
         if self.check_null == True:
             self.__check_nulls__()
+        if self.check_data_types == True:
+            self.__check_data_types__()
 
 
-df = pd.DataFrame({0: ['g', 'g', 'g', 'y', 't', 'u', 'o'],
+df = pd.DataFrame({0: [1, 2.9, 3, 4, 5, 6, 7],
                     1: ['a', 'a', 'a', 'a', 'a', 'a', 'a'],
-                    2: ['b', 'b', np.nan, 'b', 'b', 'b', 'b'],
-                    3: ['t', np.nan, 'f', 'f', 't', 'h', 'j'],
+                    2: ['b', 'b', np.NaN, 'b', 'b', 'b', 'b'],
+                    3: ['t', np.NaN, 'f', 'f', 't', 'h', 'j'],
                     4: ['f', 'h', 'd', 'k', 'j', 'y', 'j']})
 
-jw = johnWayne(df, False)
+jw = johnWayne(df)
         
     
         
