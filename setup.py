f3b5bc76e29a1a441daa03d3cee72aa4ab49d096
dustin-py <dustinstringer92@gmail.com>
2020-09-03 09:33:25 -0700

    updated version in setup.py to 0.0.5

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="lambdata-dustinstringer", # Replace with your own username
-    version="0.0.4",
+    version="0.0.5",
     author="Dustin Stringer",
     author_email="dustinstringer92@gmail.com",
     description="A small example package",
