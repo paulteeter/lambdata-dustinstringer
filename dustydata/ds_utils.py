c7d65bb1dcc5468132cf089663efbfd888371d4a
dustin-py <dustinstringer92@gmail.com>
2020-09-02 20:17:57 -0700

    add tri_split

--- dustydata/ds_utils.py
+++ dustydata/ds_utils.py
@@ -1,23 +1,23 @@
-def enlarge(n):
-    """
-    Param n is a number
-    Function will enlarge the number
-    """
-    return n * 100
-
-print(enlarge(5))
-
+from sklearn.model_selection import train_test_split
 
 def cardinality_cutter(df, threshold=50):
     """
     Cut the cardinality of categorical data.
+    Return a list of features that made the cut.
     """
     card = df.select_dtypes(exclude='number').nunique()
     card_cut = card[card <= threshold].index.tolist()
     return card_cut    
 
-if __name__ == '__main__':  # run the name space when impoted in test_functions
-    pass
+
+def tri_split(data):
+    x, y = train_test_split(data)
+    x, z = train_test_split(x)
+    return x, y, z
+
+
+# if __name__ == '__main__':  # run the name space when impoted in test_functions
+#     pass
     
     
     
